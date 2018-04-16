0b4b572cef968b111297ba3e183d69c8735eb021
Ben Farmer <ben.farmer@gmail.com>
2018-04-16 18:20:10 +0100

    Restructured things so that data shape for JointModel is interpreted the same as scipy.stats.multivariate objects (i.e. last dimension indexes components). I think there are still bugs though since the CMS example gives odd looking results.

--- tests/tests_jointmodel.py
+++ tests/tests_jointmodel.py
@@ -19,27 +19,38 @@ import numpy as np
 
 # Test basic constructions
 
-gauss2D  = jtd.JointModel([sps.norm(loc=3,scale=1), sps.norm(loc=10,scale=2)])
-gauss2Db = jtd.JointModel([sps.norm(loc=5,scale=1), sps.norm(loc=15,scale=2)])
+
+# Can't actually do this right now
+gauss2D  = jtd.JointModel([sps.norm(loc=3,scale=1), sps.norm(loc=10,scale=2)], frozen=True)
+gauss2Db = jtd.JointModel([sps.norm(loc=5,scale=1), sps.norm(loc=15,scale=2)], frozen=True)
+
+# This works though
+#gauss2D  = jtd.JointModel([sps.norm, sps.norm], [{"loc":3,"scale":1}, {"loc":10,"scale":2}])
+#gauss2Db = jtd.JointModel([sps.norm, sps.norm], [{"loc":5,"scale":1}, {"loc":15,"scale":2}])
 
 mix = jtd.MixtureModel([gauss2D,gauss2Db],[0.3,0.7])
 
+print( gauss2D.rvs(3) )
 print( mix.pdf([3,5]) )
-print( mix.rvs((5,))  )# size has to be a tuple, I didn't make a 0d special case.
 print( mix.rvs((3,4)) )
-
+print( mix.rvs(5)  )# size has to be a tuple, I didn't make a 0d special case.
 # Plots?
 
 # 2D pdf
 x = np.arange(0,10,0.1)
 y = np.arange(0,20,0.2)
 X, Y = np.meshgrid(x, y)
-PDF = mix.pdf([X,Y])
+# last dimension needs to index components
+data = np.stack([X,Y],axis=-1)
+print("data.shape:",data.shape)
+PDF = mix.pdf(data)
+print("PDF.shape:", PDF.shape)
 im = plt.imshow(PDF, interpolation='bilinear', aspect='auto',
                 origin='lower', extent=[np.min(x),np.max(x),np.min(y),np.max(y)])
 plt.show()
 
 # 2D scatter plot of random samples
-samples = mix.rvs((10000,))
-plt.scatter(*samples,lw=0,s=1)
+samples = mix.rvs(10000)
+print("samples.shape:",samples.shape)
+plt.scatter(*samples.T,lw=0,s=1)
 plt.show()
